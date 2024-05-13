d76d81d41b480e03fabb0b7a06078ad39df0f363
Oleksii Lopato <alopato@indeed.com>
2024-05-13 21:58:41 +0900

    add cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
 // TODO: Add your code here
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
